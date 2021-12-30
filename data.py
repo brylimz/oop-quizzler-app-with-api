87b6eae0f10e6989ed792bd50c439488de91c1e0
brylimz <superuser@brylimz.com>
2021-12-30 14:26:55 +0700

    fix bugs, give feedback to the player

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 import requests
 parameters = {
     "amount": 100,
-    "type": "boolean"
+    "type": "boolean",
+
 }
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
