ee590c35d57437a7c05e399125848f122273b095
brylimz <superuser@brylimz.com>
2021-12-30 13:51:25 +0700

    typing and showing in the next question in the GUI

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,9 +18,10 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=200,
             text="Some Question Text",
             fill=THEME_COLOR,
-            font=("Arial", 20, "italic"))
+            font=("Arial", 15, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
@@ -28,7 +31,10 @@ class QuizInterface:
         false_image = PhotoImage(file="images/false.png")
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
-
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
