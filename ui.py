8926a0fbc491436684b9795f4eeac5c913daa6b2
brylimz <superuser@brylimz.com>
2021-12-30 13:40:45 +0700

    UI Completed

--- ui.py
+++ ui.py
@@ -12,4 +12,23 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
+        self.canvas = Canvas(width=300, height=250, background="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+
+
         self.window.mainloop()
